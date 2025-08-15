474afb470a8f35c34ffa9d9e1f9959050112967e
Sebastian Rowan <79868066+sebastianrowan@users.noreply.github.com>
2025-08-15 16:41:39 -0400

    Update windows gdal path

--- c_windows_amd64.go
+++ c_windows_amd64.go
@@ -3,7 +3,7 @@
 package gdal
 
 /*
-#cgo windows LDFLAGS: -Lc:/gdal/release-1600-x64/lib -lgdal_i
-#cgo windows CFLAGS: -IC:/gdal/release-1600-x64/include
+#cgo windows LDFLAGS: -Lc:/OSGeo4W/lib -lgdal_i
+#cgo windows CFLAGS: -IC:/OSGeo4W/include
 */
 import "C"
